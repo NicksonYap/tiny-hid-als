6b443eb6e8f7a300dbde0e98c2018006c78e7dac
MatejKocourek <36243155+MatejKocourek@users.noreply.github.com>
2023-11-26 16:41:52 +0000

    Update usbconfig.h
    Fixes #1

--- lib/vusb/usbconfig.h
+++ lib/vusb/usbconfig.h
@@ -118,7 +118,7 @@ section at the end of this file).
 /* Define this to 1 if the device has its own power supply. Set it to 0 if the
  * device is powered from the USB bus.
  */
-#define USB_CFG_MAX_BUS_POWER           100
+#define USB_CFG_MAX_BUS_POWER           50
 /* Set this variable to the maximum USB bus power consumption of your device.
  * The value is in milliamperes. [It will be divided by two since USB
  * communicates power requirements in units of 2 mA.]
@@ -218,7 +218,7 @@ section at the end of this file).
 
 /* -------------------------- Device Description --------------------------- */
 
-#define  USB_CFG_VENDOR_ID       0xc0, 0x16 /* = 0x16c0 = 5824 = voti.nl */
+#define  USB_CFG_VENDOR_ID       0xc0, 0x16
 /* USB vendor ID for the device, low byte first. If you have registered your
  * own Vendor ID, define it here. Otherwise you may use one of obdev's free
  * shared VID/PID pairs. Be sure to read USB-IDs-for-free.txt for rules!
@@ -233,10 +233,8 @@ section at the end of this file).
 #define USB_CFG_DEVICE_VERSION  0x00, 0x01
 /* Version number of the device: Minor number first, then major number.
  */
-#define USB_CFG_VENDOR_NAME     'h', 't', 't', 'p', 's', ':', '/', '/', 'g', 'i', 't', 'h', 'u', 'b', \
-                                '.', 'c', 'o', 'm', '/', '3', 'c', 'k', 'y', '/', 't', 'i', 'n', 'y', \
-                                '-', 'h', 'i', 'd', '-', 'a', 'l', 's'
-#define USB_CFG_VENDOR_NAME_LEN 36
+#define USB_CFG_VENDOR_NAME     'M', 'a', 't', 'e', 'j',' ','K','o','c','o','u','r','e','k'
+#define USB_CFG_VENDOR_NAME_LEN 14
 /* These two values define the vendor name returned by the USB device. The name
  * must be given as a list of characters under single quotes. The characters
  * are interpreted as Unicode (UTF-16) entities.
@@ -245,14 +243,14 @@ section at the end of this file).
  * obdev's free shared VID/PID pair. See the file USB-IDs-for-free.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME     'L', 'i', 'g', 'h', 't', ' ', 'S', 'e', 'n', 's', 'o', 'r'
-#define USB_CFG_DEVICE_NAME_LEN 12
+#define USB_CFG_DEVICE_NAME     'S','p','a','r','k',' ','A','L','S'
+#define USB_CFG_DEVICE_NAME_LEN 9
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
  * you use a shared VID/PID.
  */
-/*#define USB_CFG_SERIAL_NUMBER   'N', 'o', 'n', 'e' */
-/*#define USB_CFG_SERIAL_NUMBER_LEN   0 */
+#define USB_CFG_SERIAL_NUMBER   'k', 'o', 'c', 'o', 'u', 'r', 'e', 'k','.','c','z',':','1'
+#define USB_CFG_SERIAL_NUMBER_LEN   13
 /* Same as above for the serial number. If you don't want a serial number,
  * undefine the macros.
  * It may be useful to provide the serial number through other means than at
@@ -273,7 +271,7 @@ section at the end of this file).
  * HID class is 3, no subclass and protocol required (but may be useful!)
  * CDC class is 2, use subclass 2 and protocol 1 for ACM
  */
-#define USB_CFG_HID_REPORT_DESCRIPTOR_LENGTH    316
+#define USB_CFG_HID_REPORT_DESCRIPTOR_LENGTH    348//sizeof(usbHidReportDescriptor)
 /* Define this to the length of the HID report descriptor, if you implement
  * an HID device. Otherwise don't define it or define it to 0.
  * If you use this define, you must add a PROGMEM character array named
